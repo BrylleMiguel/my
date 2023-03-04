2c10a50802a95fe57d9e394ced09b204115ef84f
Brylle M <miguelbrylle@gmail.com>
2023-03-04 18:14:15 -0500

    typewriter added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,42 @@ import Display from "./components/Display";
 import Form from "./components/Form";
 import useForm from "./hooks/useForm";
 import usePrompt from "./hooks/usePrompt";
+import Typewriter from "typewriter-effect";
 
 function App() {
     const { formInput, inputChangeHandler, clearFormData } = useForm("");
     const { display } = usePrompt(formInput);
 
+    const [isLoading, setIsLoading] = React.useState(true);
+
     return (
         <React.Fragment>
             <main className="min-w-full max-w-lg min-h-screen bg-zinc-900 text-gray-300 text-sm p-3 font-mono">
-                <Display display={display} />
-                <Form
-                    formInput={formInput}
-                    inputChangeHandler={inputChangeHandler}
-                    clearFormData={clearFormData}
-                />
+                {isLoading === true ? (
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString(
+                                    ["accessing", "system..."].join(" ")
+                                )
+                                .pauseFor(1000)
+                                .stop()
+                                .start()
+                                .callFunction(() => {
+                                    setIsLoading(false);
+                                });
+                        }}
+                    />
+                ) : (
+                    <>
+                        <Display display={display} />
+                        <Form
+                            formInput={formInput}
+                            inputChangeHandler={inputChangeHandler}
+                            clearFormData={clearFormData}
+                        />
+                    </>
+                )}
             </main>
         </React.Fragment>
     );
